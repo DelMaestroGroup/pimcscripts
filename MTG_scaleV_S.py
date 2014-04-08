ad61abd4288e824dd2bf7c9e47fd48feddda3d89
Max Graves <mtg6193@gmail.com>
2014-04-08 15:06:41 +0000

    finished MTG_scaleV_S script that plots data for film potential scaling

--- MTG_scaleV_S.py
+++ MTG_scaleV_S.py
@@ -80,11 +80,11 @@ def main():
                 arrowprops=dict(arrowstyle="->",
                     connectionstyle="angle,angleA=0,angleB=90,rad=10"),
                 )
-    '''
+    
     # plot just the bulk density as a function of chemical potential
     figg2 = pl.figure(2)
     ax2 = figg2.add_subplot(111)
-    pl.xlabel(r'$\mu\ [K]$', fontsize=20)
+    pl.xlabel(r'$\text{Potential Shift}\ [K]$', fontsize=20)
     pl.ylabel('Spatial Density '+r'$[\si{\angstrom}^{-d}]$', fontsize=20)
     pl.grid(True)
     #pl.xlim([0.5,3.0])
@@ -95,21 +95,19 @@ def main():
     yticks[0].set_visible(False)
 
     # set up bulk SVP densities for plot
-    bulkVert = -30
-    minMus = -3.0
-    maxMus = 5.0
+    bulkVert = 30   # changes text box above (positive) or below (negative) line
     boxSubtract = 1.6
     pl.plot([minMus, maxMus], [0.02198, 0.02198], 'k-', lw=3)
-    pl.annotate('3d SVP', xy=(maxMus - boxSubtract, 0.02195),  #xycoords='data',
+    pl.annotate('3d SVP', xy=(maxMus - boxSubtract, 0.02198),  #xycoords='data',
             xytext=(-50, bulkVert), textcoords='offset points',
             bbox=dict(boxstyle="round", fc="0.8"),
             arrowprops=dict(arrowstyle="->",
                 connectionstyle="angle,angleA=0,angleB=90,rad=10"),
             )
-
+    
     # number of particles in film region vs chemical potential
     pl.figure(3)
-    pl.xlabel(r'$\mu\ [K]$', fontsize=20)
+    pl.xlabel(r'$\text{Potential Shift}\ [K]$', fontsize=20)
     pl.ylabel('Number of Particles', fontsize=20)
     pl.grid(True)
     #pl.xlim([0.5,3.0])
@@ -118,10 +116,10 @@ def main():
     pl.tick_params(axis='both', which='minor', labelsize=16)
     yticks = ax.yaxis.get_major_ticks()
     yticks[0].set_visible(False)
-
+    
     # normalized angular winding vs chemical potential
     pl.figure(4)
-    pl.xlabel(r'$\mu\ [K]$', fontsize=20)
+    pl.xlabel(r'$\text{Potential Shift}\ [K]$', fontsize=20)
     pl.ylabel(r'$\Omega$', fontsize=20)
     pl.grid(True)
     pl.tick_params(axis='both', which='major', labelsize=16)
@@ -131,20 +129,32 @@ def main():
 
     # actual density in cell
     pl.figure(5)
-    pl.xlabel(r'$\mu\ [K]$', fontsize=20)
+    pl.xlabel(r'$\text{Potential Shift}\ [K]$', fontsize=20)
     pl.ylabel(r'$\rho\ [\si{\angstrom}^{-3}]$', fontsize=20)
     pl.grid(True)
     pl.tick_params(axis='both', which='major', labelsize=16)
     pl.tick_params(axis='both', which='minor', labelsize=16)
     yticks = ax.yaxis.get_major_ticks()
     yticks[0].set_visible(False)
-    '''
+ 
+    # superfluid fraction
+    pl.figure(6)
+    pl.xlabel(r'$\text{Potential Shift}\ [K]$', fontsize=20)
+    pl.ylabel(r'$\rho_S/\rho$', fontsize=20)
+    pl.grid(True)
+    pl.tick_params(axis='both', which='major', labelsize=16)
+    pl.tick_params(axis='both', which='minor', labelsize=16)
+    yticks = ax.yaxis.get_major_ticks()
+    yticks[0].set_visible(False)
+    
     # --- loop through known directory structure ------------------------------
     for nS, Sval in enumerate(sorted(Svals)):
         
         os.chdir(Sval)
 
         Vdirs = glob.glob('*V*')
+
+        print os.getcwd()
 
         # store bulk separation value
         S = re.search(r'\d+',Sval).group(0)
@@ -164,6 +174,7 @@ def main():
         # multiply angular winding by normalization to be fixed in code later 
         omegaNorm = 4.0*(float(S)+1.0*Ly)**2
 
+        # Arrays to hold all data
         Vs = pl.array([])
         Films = pl.array([])
         Bulks = pl.array([])
@@ -173,10 +184,11 @@ def main():
         omegaErrs = pl.array([])
         NumParts = pl.array([])
         NumPartErrs = pl.array([])
+        Supers = pl.array([])
+        SuperErrs = pl.array([])
 
-        n = 0
-        nw = 3
-        nn = 12
+        
+        # pass potential shift directories for current S value
         for Vdir in Vdirs:
 
             os.chdir(Vdir)
@@ -184,20 +196,21 @@ def main():
             # get bipartition file name
             f = glob.glob('*Bipart*')[0]
 
-            '''# get angular winding file name
+            # get angular winding file name
             fw = glob.glob('*Ntwind*')[0]
-
+            
             # get estimator file name (includes total number)
             fe = glob.glob('*Estimator*')[0]
-            '''
+
+            # get superfrac file name
+            fs = glob.glob('*Super*')[0]
     
+            # build array of film potential shifts from directory names
             Vs = pl.append(Vs,float(Vdir[1:])) 
 
             # --- Densities ---------------------------------------------------
             filmavg,filmstd,filmbins,bulkavg,bulkstd,bulkbins = pl.genfromtxt(f,
                     unpack=True, usecols=(0,1,2,3,4,5), delimiter=',')
-                    #unpack=True, usecols=(n,n+1,n+2,n+3,n+4,n+5), delimiter=',')
-            #n += 6
             
             # get rid of any items which are not numbers..
             # this is some beautiful Python juju.
@@ -226,11 +239,10 @@ def main():
             filmErrs = pl.append(filmErrs, filmstdErr)
             bulkErrs = pl.append(bulkErrs, bulkstdErr)
 
-            '''
+            
             # ---- angular winding --------------------------------------------
             omegaAvg,omegaStd,omegaBins = pl.genfromtxt(fw,
-                    unpack=True, usecols=(nw,nw+1,nw+2), delimiter=',')
-            nw += 9
+                    unpack=True, usecols=(3,4,5), delimiter=',')
 
             # get rid of any items which are not numbers..
             omegaBins = omegaBins[pl.logical_not(pl.isnan(omegaBins))]
@@ -251,11 +263,11 @@ def main():
 
             Omegas = pl.append(Omegas, Omega)
             omegaErrs = pl.append(omegaErrs, omegaErr)
-            
+           
+
             # ---- total number -----------------------------------------------
             numAvg,numStd,numBins = pl.genfromtxt(fe,
-                    unpack=True, usecols=(nn,nn+1,nn+2), delimiter=',')
-            nn += 15
+                    unpack=True, usecols=(12,13,14), delimiter=',')
 
             # get rid of any items which are not numbers..
             numBins = numBins[pl.logical_not(pl.isnan(numBins))]
@@ -276,17 +288,41 @@ def main():
 
             NumParts = pl.append(NumParts, numPart)
             NumPartErrs = pl.append(NumPartErrs, numPartErr)
-            '''
+
+
+            # ---- superfluid fraction ----------------------------------------
+            supAvg,supStd,supBins = pl.genfromtxt(fs,
+                    unpack=True, usecols=(0,1,2), delimiter=',')
+
+            # get rid of any items which are not numbers..
+            supBins = supBins[pl.logical_not(pl.isnan(supBins))]
+            supStd = supStd[pl.logical_not(pl.isnan(supStd))]
+            supAvg = supAvg[pl.logical_not(pl.isnan(supAvg))]
+
+            # normalize data.
+            #supStd /= (1.0*accessibleVol)
+            #supAvg /= (1.0*accessibleVol)
+
+            weights = supBins/pl.sum(supBins)
+
+            supAvg *= weights
+            supStd *= weights
+
+            supPart = pl.sum(supAvg)
+            supPartErr = pl.sum(supStd)
+
+            Supers = pl.append(Supers, supPart)
+            SuperErrs = pl.append(SuperErrs, supPartErr)
+
 
             os.chdir('..')
 
+
         # Sort data in order of increasing chemical potential. I love Python.
         # This is another bit of magical Python juju.
-        #Vs, Films, Bulks, filmErrs, bulkErrs, Omegas, omegaErrs, NumParts, NumPartErrs = pl.asarray(
-        #        zip(*sorted(zip(Vs, Films, Bulks, filmErrs, bulkErrs, 
-        #            Omegas, omegaErrs, NumParts, NumPartErrs))))
-        Vs, Films, Bulks, filmErrs, bulkErrs = pl.asarray(
-                zip(*sorted(zip(Vs, Films, Bulks, filmErrs, bulkErrs))))
+        Vs, Films, Bulks, filmErrs, bulkErrs, Omegas, omegaErrs, NumParts, NumPartErrs, Supers, SuperErrs = pl.asarray(
+                zip(*sorted(zip(Vs, Films, Bulks, filmErrs, bulkErrs, 
+                    Omegas, omegaErrs, NumParts, NumPartErrs, Supers, SuperErrs))))
 
 
         pl.figure(1) 
@@ -297,43 +333,48 @@ def main():
                 label=labell+', 3d',
                 markersize=8)
 
-        '''
         pl.figure(2) 
-        pl.errorbar(mus, Bulks, bulkErrs, fmt = '-d', color=colors[nS],
+        pl.errorbar(Vs, Bulks, bulkErrs, fmt = '-d', color=colors[nS],
                 label=labell+', 3d',
                 markersize=8)
 
         pl.figure(3)
-        pl.errorbar(mus, Films*projArea, fmt='-o', color=colors[nS],
+        pl.errorbar(Vs, Films*projArea, fmt='-o', color=colors[nS],
                 label = labell+', 2d',
                 markersize=8)
 
         pl.figure(4)
-        pl.errorbar(mus, Omegas, omegaErrs, fmt='-o', color=colors[nS],
+        pl.errorbar(Vs, Omegas, omegaErrs, fmt='-o', color=colors[nS],
                 label = labell, markersize=8)
 
         pl.figure(5)
-        pl.errorbar(mus, NumParts, NumPartErrs, fmt='-o', color=colors[nS],
+        pl.errorbar(Vs, NumParts, NumPartErrs, fmt='-o', color=colors[nS],
                 label = labell, markersize=8)
-        '''
+ 
+        pl.figure(6)
+        pl.errorbar(Vs, Supers, SuperErrs, fmt='-o', color=colors[nS],
+                label = labell, markersize=8)
+       
         os.chdir('..')
 
    
     pl.figure(1)
-    pl.legend(loc=2)
-    '''
+    pl.legend(loc=1)
+    
     pl.figure(2)
-    pl.legend(loc=2)
+    pl.legend(loc=1)
  
     pl.figure(3)
-    pl.legend(loc=2)
+    pl.legend(loc=1)
   
     pl.figure(4)
-    pl.legend(loc=2)
+    pl.legend(loc=1)
    
     pl.figure(5)
-    pl.legend(loc=2)
-    '''
+    pl.legend(loc=1)
+
+    pl.figure(6)
+    pl.legend(loc=1)
   
     #pl.savefig('density_vs_mu_allAlphas.pdf', format='pdf',
     #        bbox_inches='tight')
